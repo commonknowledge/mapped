2367ee2879e5b2e30c7c9463478f087a797c672a
Arbyhisenaj <41119392+Arbyhisenaj@users.noreply.github.com>
2024-12-20 12:48:11 +0000

    Update ReportNavbar.tsx

--- nextjs/src/app/reports/[id]/(components)/ReportNavbar.tsx
+++ nextjs/src/app/reports/[id]/(components)/ReportNavbar.tsx
@@ -1,12 +1,11 @@
 import ReportActions from '@/app/reports/[id]/(components)/ReportActions'
 import { useReport } from '@/app/reports/[id]/(components)/ReportProvider'
 import { useSidebar } from '@/components/ui/sidebar'
-import { useParams } from 'next/navigation'
+import { useParams, usePathname } from 'next/navigation'
 
-import { Button } from '@/components/ui/button'
 import { contentEditableMutation } from '@/lib/html'
 import { atom, useAtomValue } from 'jotai'
-import { PanelRight } from 'lucide-react'
+import { BarChartIcon, MapIcon, PanelRight } from 'lucide-react'
 import Link from 'next/link'
 import { MappedIcon } from '../../../../components/icons/MappedIcon'
 
@@ -14,11 +13,14 @@ type Params = {
   id: string
 }
 
+const activeTabAtom = atom('map')
+
 // You can set the title & href of the top left icon link based on route & context
 export const navbarTitleAtom = atom('')
 export const NAVBAR_HEIGHT = 48
 
 export default function ReportNavbar() {
+  const pathname = usePathname()
   const params = useParams()
   const id = params.id
   const title = useAtomValue(navbarTitleAtom)
@@ -48,15 +50,39 @@ export default function ReportNavbar() {
             className="text-meepGray-400 w-4 h-4 cursor-pointer"
           />{' '}
         </div>
+        <div className="flex flex-row items-center gap-2 px-10">
+          {navbarTabs(id as string).map((tab) => (
+            <Link href={tab.href} key={tab.name}>
+              <div
+                className={`py-1 px-2 font-normal flex flex-row items-center gap-2 rounded-md ${
+                  pathname === tab.href
+                    ? 'bg-meepGray-800 opacity-100'
+                    : 'hover:bg-meepGray-800 opacity-50'
+                }`}
+              >
+                {tab.icon}
+                {tab.name}
+              </div>
+            </Link>
+          ))}
+        </div>
       </section>
-      <section className="flex space-x-4">
-        <Link href={`/reports/${id}`}>
-          <Button>Map</Button>
-        </Link>
-        <Link href={`/reports/${id}/dashboard`}>
-          <Button>Dashboard</Button>
-        </Link>
-      </section>
+      <section className="flex space-x-4"></section>
     </nav>
   )
+}
+
+function navbarTabs(id: string) {
+  return [
+    {
+      name: 'Map',
+      href: `/reports/${id}`,
+      icon: <MapIcon className="w-4 h-4" />,
+    },
+    {
+      name: 'Dashboard',
+      href: `/reports/${id}/dashboard`,
+      icon: <BarChartIcon className="w-4 h-4" />,
+    },
+  ]
 }
