280d9180cfe12b3a19f55775ed8728b048fd2988
Jan Baykara <jan@commonknowledge.coop>
2025-02-21 23:53:47 +0000

    Does this let the TestCase serve an ngrok tunnel?

--- hub/tests/test_external_data_source_integrations.py
+++ hub/tests/test_external_data_source_integrations.py
@@ -9,7 +9,7 @@ from unittest import skip, skipIf
 from django.conf import settings
 from django.core.files import File
 from django.db.utils import IntegrityError
-from django.test import TestCase
+from django.test import LiveServerTestCase, TestCase
 
 from asgiref.sync import async_to_sync, sync_to_async
 
@@ -23,6 +23,7 @@ class TestExternalDataSource:
     lockfile = __file__
     constituency_field = "constituency"
     mayoral_field = "mayoral region"
+    port = settings.TEST_SERVER_PORT
 
     def setUp(self: TestGraphQLClientCase) -> None:
         super().setUp()
@@ -780,7 +781,9 @@ class TestExternalDataSource:
     settings.SKIP_AIRTABLE_TESTS,
     "Skipping Airtable tests",
 )
-class TestAirtableSource(TestExternalDataSource, TestGraphQLClientCase):
+class TestAirtableSource(
+    TestExternalDataSource, TestGraphQLClientCase, LiveServerTestCase
+):
     def create_test_source(self, name="My test Airtable member list"):
         self.source = models.AirtableSource.objects.create(
             name=name,
@@ -810,7 +813,9 @@ class TestAirtableSource(TestExternalDataSource, TestGraphQLClientCase):
         return self.source
 
 
-class TestMailchimpSource(TestExternalDataSource, TestGraphQLClientCase):
+class TestMailchimpSource(
+    TestExternalDataSource, TestGraphQLClientCase, LiveServerTestCase
+):
     constituency_field = "CONSTITUEN"
     mayoral_field = "MAYORAL_RE"
 
@@ -842,7 +847,9 @@ class TestMailchimpSource(TestExternalDataSource, TestGraphQLClientCase):
         return self.source
 
 
-class TestActionNetworkSource(TestExternalDataSource, TestGraphQLClientCase):
+class TestActionNetworkSource(
+    TestExternalDataSource, TestGraphQLClientCase, LiveServerTestCase
+):
     constituency_field = "custom_fields.constituency"
     mayoral_field = "custom_fields.mayoral_region"
 
@@ -914,7 +921,9 @@ class TestActionNetworkSource(TestExternalDataSource, TestGraphQLClientCase):
 @skip(
     reason="Google Sheets can't be automatically tested as the refresh token expires after 7 days - need to use a published app"
 )
-class TestEditableGoogleSheetsSource(TestExternalDataSource, TestGraphQLClientCase):
+class TestEditableGoogleSheetsSource(
+    TestExternalDataSource, TestGraphQLClientCase, LiveServerTestCase
+):
     def create_test_source(self, name="My test Google member list"):
         self.source: models.EditableGoogleSheetsSource = (
             models.EditableGoogleSheetsSource.objects.create(
