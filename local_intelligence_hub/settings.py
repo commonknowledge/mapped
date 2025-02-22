b4decb233f5002a189b300f8b79016c2f67c7ff6
Jan Baykara <jan@commonknowledge.coop>
2025-02-22 00:30:10 +0000

    More logging / debugging

--- local_intelligence_hub/settings.py
+++ local_intelligence_hub/settings.py
@@ -151,6 +151,8 @@ FRONTEND_BASE_URL = (
 BACKEND_URL = env("BASE_URL") if ENVIRONMENT != "production" else env("PROD_BASE_URL")
 BASE_URL = BACKEND_URL
 # Network security
+print("----BASE_URL", env("BASE_URL"), env("PROD_BASE_URL"))
+print("----ALLOWED_HOSTS", env("ALLOWED_HOSTS"), env("PROD_ALLOWED_HOSTS"))
 ALLOWED_HOSTS = (
     env("ALLOWED_HOSTS") if ENVIRONMENT != "production" else env("PROD_ALLOWED_HOSTS")
 )
