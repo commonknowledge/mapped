643b077a36c81813181a14ba3cd0725dd0b176ba
ev <4164774+ev-sc@users.noreply.github.com>
2025-03-10 14:28:03 +0000

    Generate geocoding config from column

---
+++ nextjs/src/app/(logged-in)/data-sources/inspect/[externalDataSourceId]/getGeocodingConfigFromGeographyColumn.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { GeographyTypes } from '@/__generated__/graphql'
+import { HubAreaType } from '@/app/reports/[id]/politicalTilesets'
+
+export function getGeocodingConfigFromGeographyColumn(
+  geographyColumn: string,
+  geographyColumnType: GeographyTypes
+):
+  | {
+      type: 'AREA'
+      components: [
+        {
+          type: 'area_code'
+          field: string
+          value: ''
+          metadata: { lih_area_type__code: HubAreaType }
+        },
+      ]
+    }
+  | {} {
+  const geocodingConfig = {
+    type: 'AREA',
+    components: [
+      {
+        type: 'area_code',
+        field: geographyColumn,
+        value: '',
+        metadata: { lih_area_type__code: '' },
+      },
+    ],
+  }
+
+  if (geographyColumnType === 'PARLIAMENTARY_CONSTITUENCY_2024') {
+    geocodingConfig.components[0].metadata.lih_area_type__code = 'WMC23'
+    return geocodingConfig
+  } else if (geographyColumnType === 'WARD') {
+    geocodingConfig.components[0].metadata.lih_area_type__code = 'WD23'
+    return geocodingConfig
+  } else if (geographyColumnType === 'ADMIN_DISTRICT') {
+    return {}
+  } else if (geographyColumnType === 'POSTCODE') {
+    return {}
+  } else if (geographyColumnType === 'ADDRESS') {
+    return {}
+  } else if (geographyColumnType === 'OUTPUT_AREA') {
+    return {}
+  } else return {}
+}
